a96e38ce5514892cb256afbdf7e4c270e1479ca7
Chloe Steylaers <csteylae@mi-r4-p4.s19.be>
2024-08-07 17:22:36 +0200

    Progress

--- inc/minitry_struct.h
+++ inc/minitry_struct.h
@@ -6,7 +6,7 @@
 /*   By: iwaslet <iwaslet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 12:20:42 by csteylae          #+#    #+#             */
-/*   Updated: 2024/08/07 14:48:33 by csteylae         ###   ########.fr       */
+/*   Updated: 2024/08/07 17:22:22 by csteylae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,9 +58,10 @@ typedef struct s_redirect
 }   t_redirect;
 
 /**
- * A structure containing the commands that will by passed to the executor in form of an array of t_command
+ * A structure containing the commands that will be passed to the execution
+ * in form of an array of t_command
  *
- * - command->cmd	: 				an array of strings, as char **argv. It	contains in cmd[0] the command name and the others indexes contains its potential options
+ * - command->cmd	: 				an array of strings, as char **argv. Contains in cmd[0] the command name and the others indexes contains its potential options
  * - command->in (optionnal)	: 	an array of struct t_redirect. Contains all the possible IN_REDIR (noted <) that can affect a command
  * - command->out (optionnal)	:	an array of struct t_redirect. Contains all the possible OUT_REDIR (noted >) that can affect a cmd
  * - command->heredoc (optionnal) : an array of struct t_redirect. Contains all the possible HEREDOC_REDIR (noted << delimitor_name). In that case, the redirect->heredoc_delimiter is not NULL and indicate the name of the delimiter.
@@ -68,36 +69,38 @@ typedef struct s_redirect
  */
 typedef struct s_command
 {
-    char        **cmd;
-    t_redirect  *in;
-    t_redirect  *out;
-    t_redirect  *heredoc;
-    t_redirect  *append;
-}   t_command;
+	char		**cmd;
+	t_redirect	*in;
+	t_redirect	*out;
+	t_redirect	*heredoc;
+	t_redirect	*append;
+}	t_command;
 
 /**
- *	A big structure that will contains all important information to parse from parsing to execution. 
+ *	A big structure that will contains all important information
  *
  *	shell.envp	contains the envp variable
  *	shell.tab	contains the array of commands parsed from readline()
- *	shell.tab 	the size of the array. Is the nb of commands passed
+ *	shell.tab_size 	the size of the array. Is the nb of commands passed
+ *	shell.error	an int to indicate if an error occurs, 
+ *		idk if we will keep it (just use in init_shell() in src/parsing/minitry.c
  */
 typedef struct s_shell
 {
-    char		**env;
-    t_command	*tab;
+	char		**env;
+	t_command	*tab;
 	int			tab_size;
-	int			error; //still need to be constructed. To indicate if an error has occured
-}   t_shell;
+	int			error;
+}	t_shell;
 
 typedef struct s_array
 {
-    void    *content;
-    size_t  max_size;
-    size_t  type_size;
-    size_t  size;
-    size_t  nbr;
-    size_t  block;
-}       t_array;
+	void	*content;
+	size_t	max_size;
+	size_t	type_size;
+	size_t	size;
+	size_t	nbr;
+	size_t	block;
+}	t_array;
 
-# endif
+#endif
