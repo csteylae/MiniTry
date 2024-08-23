080788911955063cdc65a2c085f06eb566e8bed3
Chloe Steylaers <csteylae@fu-r6-p7.s19.be>
2024-08-23 14:02:12 +0200

    Progress

--- src/execution/exec_prompt.c
+++ src/execution/exec_prompt.c
@@ -6,7 +6,7 @@
 /*   By: csteylae <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 16:07:13 by csteylae          #+#    #+#             */
-/*   Updated: 2024/08/07 14:51:28 by csteylae         ###   ########.fr       */
+/*   Updated: 2024/08/23 14:00:52 by csteylae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@ int	exec_prompt(t_shell *shell)
 {
 	int	i;
 	int	pid;
-	int	*fd;
+	//int	*fd;
 //	int	pipe_fd[2];
 
-	if (shell->tab_size > 1)
-		exec_pipeline(shell);
+//	if (shell->tab_size > 1)
+//		exec_pipeline(shell);
 	//check if pipeline
 	// else check if redirections
 	// check if builtins
 	//if not pipeline nor builtin, idk just try to exec somthing ? Should we check if its just a variable assignment ?
+	i = 0;
 	pid = fork();
 	if (pid < 0)
-	exit_error(shell, "fork");
-	if (pid[i] == 0)
-		exec_command(shell, i);
+		exit_error(shell, "fork");
+	if (pid == 0)
+		exec_command(shell, 0);
 	wait(NULL);
 	//need to register the exit status of last process should be attached to shell
 	free_tab_cmd(shell->tab_size, shell->tab);
