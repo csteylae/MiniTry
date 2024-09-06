29d8198e40a0040ec26ce7efbb55284d6da5a77e
Chloe Steylaers <csteylae@mi-r3-p3.s19.be>
2024-09-06 18:37:52 +0200

    Progress

--- src/execution/exec_pipeline.c
+++ src/execution/exec_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: csteylae <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 13:37:24 by csteylae          #+#    #+#             */
-/*   Updated: 2024/09/06 14:56:18 by csteylae         ###   ########.fr       */
+/*   Updated: 2024/09/06 15:52:23 by csteylae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,26 @@
 
 static void	redirect_io(t_shell *shell, int *new_fd_in, int *new_fd_out)
 {
-	if (new_fd_in >= 0)
+	ft_printf("WTHHHHHHHHHHHH\n");
+	if (*new_fd_in >= 0)
 	{
 		if (dup2(*new_fd_in, STDIN_FILENO) < 0)
+		{
+			ft_printf("yeppppppppppp\n");
 			exit_error(shell, "dup2");
+		}
 		close(*new_fd_in);
+		ft_printf("fd_in redirected \n");
 	}
-	if (new_fd_out >= 0)
+	if (*new_fd_out >= 0)
 	{
 		if (dup2(*new_fd_out, STDOUT_FILENO) < 0)
+		{
+			ft_printf("ptttttttttttttttn\n");
 			exit_error(shell, "dup2");
+		}
 		close(*new_fd_out);
+		ft_printf("fd_out redirected\n");
 	}
 	ft_printf("no prb with dup2\n\n");
 }
@@ -54,20 +63,24 @@ static void	redirect_pipeline(t_shell *shell, int i, int pipe_fd[2], int *fd_pre
 	last_cmd = shell->tab_size - 1;
 	if (i == first_cmd)
 	{
+		ft_printf("ok1\n");
 		close(pipe_fd[READ_FROM]);
 		redirect_io(shell, fd_prev, &pipe_fd[WRITE_TO]);
 	}
 	else if (i == last_cmd)
 	{
+		ft_printf("ok end\n");
 		close(pipe_fd[READ_FROM]);
 		close(pipe_fd[WRITE_TO]);
 		redirect_io(shell, fd_prev, (int*)STDOUT_FILENO);
 	}
 	else
 	{
+		ft_printf("ok mid\n");
 		close(pipe_fd[READ_FROM]);
 		redirect_io(shell, fd_prev, &pipe_fd[WRITE_TO]);
 	}
+	ft_printf("okkkkkkkkkkkkk\n");
 }
 
 static void wait_children(pid_t *child_pid, int child_nb)
@@ -99,6 +112,7 @@ void	exec_pipeline(t_shell *shell)
 	while (i != shell->tab_size) //while command arent executed
 	{
 		//perform_redirections_files()
+		ft_printf("nb of iteration %i\n", i);
 		pipe(pipe_fd);
 		if (pipe_fd < 0)
 			exit_error(shell, "pipe");
@@ -109,7 +123,7 @@ void	exec_pipeline(t_shell *shell)
 		{
 			// prb because i passed value and no pointer so what happens in redirect_pipeline doesnt affect what will be done in exec_cmd ?
 			redirect_pipeline(shell, i, pipe_fd, &prev_fd);
-			ft_printf("is it dup ? : %d\n", STDIN_FILENO);
+			ft_printf("out of redirect_pipeline\n");
 			exec_command(shell, i);
 		}
 		close(pipe_fd[WRITE_TO]);
@@ -119,5 +133,4 @@ void	exec_pipeline(t_shell *shell)
 	}
 	close(prev_fd);
 	wait_children(child_pid, i);
-	ft_printf("has waited\n");
 }
